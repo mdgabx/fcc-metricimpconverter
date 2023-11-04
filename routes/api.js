26828f732c9fd8824a16f13d3dae08426b2f6e5d
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2023-11-04 20:18:17 +0800

    adding the error handling

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@
 
 const expect = require('chai').expect;
 const ConvertHandler = require('../controllers/convertHandler.js');
+const { init } = require('../server.js');
 
 module.exports = function (app) {
   
@@ -14,7 +15,15 @@ module.exports = function (app) {
     let initUnit = convertHandler.getUnit(input)
     let returnNum = convertHandler.convert(initNum, initUnit)
     let returnUnit = convertHandler.getReturnUnit(initUnit)
-    
+
+    if(!initNum && !initUnit) {
+      res.send("invalid number and unit")
+    } else if (!initNum) {
+      res.send("invalid number")
+    } else if (!initUnit) {
+      res.send("invalid unit")
+    }
+
 
       return res.json({ 
         initNum,
