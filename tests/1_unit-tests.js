c46c086e4c0df1311f33d54d260ae233cc7a1e61
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2023-11-19 14:51:39 +0800

    16 unit test added

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -66,14 +66,91 @@ suite('Unit Tests', function(){
 
             done();
         })
-    
-        test("convertHandler should correctly convert gal to L", function () {
+
+        test("convertHandler should return the correct return unit for each valid input unit.", function (done) {
+            let input = ["gal", "L", "lbs", "kg", "mi", "km"]
+            let output = ["L", "gal", "kg", "lbs", "km", "mi"]     
+            
+            input.forEach(function(el, index) {
+                assert.equal(convertHandler.getReturnUnit(el), output[index])
+            })
+
+            done();
+        })
+
+        test("convertHandler should correctly return the spelled-out string unit for each valid input unit.", function(done) {
+            let input = ["gal", "L", "lbs", "kg", "mi", "km"]
+            let output = ["gallons", "liters", "pounds", "kilograms", "miles", "kilometers"]
+
+            input.forEach(function(el, index) {
+                assert.equal(convertHandler.spellOutUnit(el), output[index])
+            })
+
+            done();
+        })
+
+        test("convertHandler should correctly convert gal to L", function (done) {
             let input = "4.2gal";
             let initNum = convertHandler.getNum(input)
             let initUnit = convertHandler.getUnit(input)
             let result = convertHandler.convert(initNum, initUnit)
-            assert.equal(result, "15.89872", "wrong convertion")
+            assert.equal(result, "15.89872", "gal to L convertion error")
+
+            done();
     
+        })
+
+        test("convertHandler should correctly convert L to gal.", function (done) {
+            let input = "3L";
+            let initNum = convertHandler.getNum(input)
+            let initUnit = convertHandler.getUnit(input)
+            let result = convertHandler.convert(initNum, initUnit)
+            assert.equal(result, "0.79252", "L to gal convertion error")
+
+            done();
+        })
+
+
+        test("convertHandler should correctly convert mi to km.", function (done) {
+            let input = "1.2mi";
+            let initNum = convertHandler.getNum(input)
+            let initUnit = convertHandler.getUnit(input)
+            let result = convertHandler.convert(initNum, initUnit)
+            assert.equal(result, "1.93121", "mi to km convertion error")
+
+            done();
+        })
+
+        test("convertHandler should correctly convert km to mi.", function (done) {
+            let input = "3/5km"
+            let initNum = convertHandler.getNum(input)
+            let initUnit = convertHandler.getUnit(input)
+            let result = convertHandler.convert(initNum, initUnit)
+            assert.equal(result, "0.37282", "km to mi convertion error.")
+
+            done();
+        })
+
+        test("convertHandler should correctly convert lbs to kg.", function (done) {
+            let input = "5.4/3lbs"
+            let initNum = convertHandler.getNum(input)
+            let initUnit = convertHandler.getUnit(input)
+            let result = convertHandler.convert(initNum, initUnit)
+            assert.equal(result, "0.81647", "lbs to kg convertion error.")
+
+            done();
+        })        
+
+        test("convertHandler should correctly convert kg to lbs.", function (done) {
+            let input = "1kg";
+            let initNum = convertHandler.getNum(input)
+            let initUnit = convertHandler.getUnit(input)
+            let result = convertHandler.convert(initNum, initUnit)
+            assert.equal(result, "2.20462", "kg to lbs convertion error.")
+
+            assert.equal(result, "2.20462", "kg to lbs convertion error")
+
+            done();
         })
     
     })
